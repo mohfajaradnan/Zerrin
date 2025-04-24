3aa7531dc56fd5048aba88d3c142075a90798d5b
mohfajaradnan <82563682+mohfajaradnan@users.noreply.github.com>
2025-04-24 17:48:16 +0700

    Update swiper.js

--- js/components/swiper.js
+++ js/components/swiper.js
@@ -1,47 +1,50 @@
-$(window).load(function(){
+$(window).on('load', function() {
     // Swiper Clients
-    var swiper = new Swiper('.swiper-clients', {
-    slidesPerView: 5,
-    spaceBetween: 50,
-    loop: true,
-    speed: 3000, // lebih cepat geraknya
-    autoplay: {
-        delay: 0, // delay 0 biar terus jalan
-        disableOnInteraction: false
-    },
-    freeMode: true,
-    freeModeMomentum: false,
-
-    breakpoints: {
-        1024: {
-            slidesPerView: 4,
-            spaceBetween: 50
-        },
-        992: {
-            slidesPerView: 3,
-            spaceBetween: 40
-        },
-        768: {
-            slidesPerView: 3,
-            spaceBetween: 30
-        },
-        600: {
-            slidesPerView: 2,
-            spaceBetween: 30
-        },
-        480: {
-            slidesPerView: 1,
-            spaceBetween: 0
-        }
-    }
-});
-
-
-    // Swiper Clients
-    var swiper = new Swiper('.swiper-testimonials', {
-        speed: 1000,
-        autoplay: 10000,
-        slidesPerView: 1,
+    var swiperClients = new Swiper('.swiper-clients', {
+        slidesPerView: 5,
+        spaceBetween: 50,
         loop: true,
+        speed: 3000,
+        autoplay: {
+            delay: 0,
+            disableOnInteraction: false
+        },
+        freeMode: true,
+        freeModeMomentum: false,
+        grabCursor: true,
+        allowTouchMove: false,
+
+        breakpoints: {
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 50
+            },
+            992: {
+                slidesPerView: 3,
+                spaceBetween: 40
+            },
+            768: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+            600: {
+                slidesPerView: 2,
+                spaceBetween: 30
+            },
+            480: {
+                slidesPerView: 1,
+                spaceBetween: 0
+            }
+        }
+    });
+
+    // Swiper Testimonials
+    var swiperTestimonials = new Swiper('.swiper-testimonials', {
+        speed: 1000,
+        autoplay: {
+            delay: 10000
+        },
+        slidesPerView: 1,
+        loop: true
     });
 });
